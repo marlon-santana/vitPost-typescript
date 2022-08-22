b293f1c37f313f7ddd9b0e9966dde7820df5057c
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-08-21 23:25:48 -0300

    feat/add types of components

--- src/component/Avatar.tsx
+++ src/component/Avatar.tsx
@@ -1,9 +1,18 @@
 import style from './avatar.module.css';
 
-export function Avatar({hasBorder = true, src}) {
+interface AvatarProps {
+    hasBorder?: boolean;
+    src: string;
+    alt?: string;
+}
+
+export function Avatar({hasBorder = true, src, alt}: AvatarProps) {
     return (
         <img className={hasBorder ? style.avatarWithBorder : style.avatar } 
-        src={src}/>
+        src={src}
+        alt={alt}
+        />
+        
     );
 
 }
